00e3095cde051ad9b3b5b0e2b22078fe74119ef8
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-04-03 20:22:20 +0900

    refactor: DodamFilledButton 수정

--- packages/components/src/ui/Button/FilledButton/DodamFilledButton.tsx
+++ packages/components/src/ui/Button/FilledButton/DodamFilledButton.tsx
@@ -1,66 +1,87 @@
 import { DodamShape, DodamTypography, ShapeSizeType } from "@dds-web/styles";
-import React from "react";
-import { DodamFilledButtonProps } from "./type";
+import React, { HTMLAttributes, ReactNode } from "react";
 import styled, { CSSProperties, RuleSet, css } from "styled-components";
 import { FlexLayout } from "../../../layout";
 
+type FilledColorsType = {
+  contentColor?: CSSProperties["color"];
+  contentBackgroundColor?: CSSProperties["backgroundColor"];
+  disabledColor?: CSSProperties["color"];
+  disabledBackgroundColor?: CSSProperties["backgroundColor"];
+};
+
+export interface DodamFilledButtonProps
+  extends HTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+
+  colors?: FilledColorsType;
+  radius?: ShapeSizeType;
+  padding?: CSSProperties["padding"];
+  isLoading?: boolean;
+  isDisabled?: boolean;
+  customStyle?: RuleSet;
+}
+
 export const DodamFilledButton = ({
-  text: { color, content },
-  backgroundColor,
+  children,
+  colors,
   isLoading = false,
   radius = "Large",
   padding = "16px 24px",
-  disabled = false,
+  isDisabled = false,
   customStyle,
   ...props
 }: DodamFilledButtonProps) => {
   return (
     <StyledFilledButton
-      textColor={color!}
-      backgroundColor={backgroundColor}
+      colors={colors!}
       radius={radius}
-      disabled={disabled}
+      isDisabled={isDisabled}
       isLoading={isLoading}
       padding={padding}
       customStyle={customStyle!}
-      {...(!disabled && props)}
+      {...(!isDisabled && props)}
     >
       {isLoading ? (
         <LoadingEllipseWrap>
           {Array.from({ length: 3 }).map((_, idx) => (
-            <LoadingEllipse key={idx} />
+            <LoadingEllipseItem key={idx} />
           ))}
         </LoadingEllipseWrap>
       ) : (
-        content
+        <>{children}</>
       )}
     </StyledFilledButton>
   );
 };
 
 const StyledFilledButton = styled.button<{
-  textColor: string;
-  backgroundColor: CSSProperties["backgroundColor"];
+  colors: FilledColorsType;
   radius: ShapeSizeType;
-  disabled: boolean;
+  isDisabled: boolean;
   padding: CSSProperties["padding"];
   isLoading: boolean;
   customStyle: RuleSet;
 }>`
+  width: 380px;
   min-height: 58px;
   padding: ${({ padding }) => padding};
 
-  background-color: ${({ backgroundColor, theme }) =>
-    backgroundColor || theme.primary};
-  color: ${({ textColor, theme }) => textColor || theme.onPrimary};
-  opacity: ${({ disabled, isLoading }) => (disabled || isLoading) && "0.5"};
+  opacity: ${({ isLoading }) => isLoading && "0.5"};
 
   outline: none;
   border: none;
   cursor: pointer;
 
-  transition: all 0.15s ease-in-out;
+  ${({ colors, isDisabled, theme }) => css`
+    background-color: ${(isDisabled
+      ? colors?.disabledBackgroundColor
+      : colors?.contentBackgroundColor) || theme.primary};
+    color: ${(isDisabled ? colors?.disabledColor : colors?.contentColor) ||
+    theme.onPrimary};
+  `}
 
+  transition: all 0.15s ease-in-out;
   &:active {
     ${({ disabled }) =>
       !disabled &&
@@ -80,7 +101,7 @@ const LoadingEllipseWrap = styled.div`
   ${FlexLayout({ columnGap: "8px" })}
 `;
 
-const LoadingEllipse = styled.div`
+const LoadingEllipseItem = styled.div`
   width: 8px;
   height: 8px;
   background-color: #fff;
