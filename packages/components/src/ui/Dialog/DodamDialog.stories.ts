ad077f4fe7eb75e560c3ee4e1576f265fbe0baa7
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-04-06 00:37:31 +0900

    feat: DodamDialog 구현

--- packages/components/src/ui/Dialog/DodamDialog.stories.ts
+++ packages/components/src/ui/Dialog/DodamDialog.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { DodamDialog } from "./DodamDialog";
+import { css } from "styled-components";
 
 const meta = {
   title: "ui/DodamDialog",
@@ -14,18 +15,57 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const AlertText = css`
+  cursor: pointer;
+  padding: 8px 12px;
+  color: ${({ theme }) => theme.primary};
+`;
+
 export const AlertDialog: Story = {
   args: {
     title: "제목을 입력해주세요",
-    content: "본문을 입력해주세요",
-    dialogType: "ALERT",
+    text: "본문을 입력해주세요",
+
+    type: {
+      dialog: "ALERT",
+      close: {
+        content: "닫기",
+        onClick: () => window.alert("닫기"),
+        style: AlertText,
+      },
+    },
   },
 };
 
+const StyledButton = css`
+  width: 50%;
+  height: 50px;
+  min-height: 50px;
+`;
+
+const DismissButton = css`
+  color: ${({ theme }) => theme.onSurface};
+  background-color: ${({ theme }) => theme.secondaryContainer};
+  ${StyledButton}
+`;
+
 export const ConfirmDialog: Story = {
   args: {
-    title: "제목을 입력해주세요",
-    content: "본문을 입력해주세요",
-    dialogType: "CONFIRM",
+    title: "제목을 입력해주세요.",
+    text: "본문을 입력해주세요",
+
+    type: {
+      dialog: "CONFIRM",
+      confirm: {
+        content: "확인",
+        onClick: () => window.alert("확인"),
+        style: StyledButton,
+      },
+      dismiss: {
+        content: "취소",
+        onClick: () => window.alert("취소"),
+        style: DismissButton,
+      },
+    },
   },
 };
--- packages/components/src/ui/Dialog/DodamDialog.tsx
+++ packages/components/src/ui/Dialog/DodamDialog.tsx
@@ -1,15 +1,101 @@
+import { DodamShape, ShapeSizeType } from "@dds-web/styles";
 import React from "react";
+import styled, { RuleSet, css } from "styled-components";
+import { Column, FlexLayout, Row } from "../../layout";
+import { DodamBody, DodamTitle } from "../Typography";
+import { DodamFilledButton } from "../Button";
+
+type DialogHandlerType = {
+  content: string;
+  onClick: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+  style?: RuleSet;
+};
+
+type DialogType =
+  | {
+      dialog: "ALERT";
+      close: DialogHandlerType;
+    }
+  | {
+      dialog: "CONFIRM";
+      confirm: DialogHandlerType;
+      dismiss: DialogHandlerType;
+    };
 
 export interface DodamDialogProps {
   title: string;
-  content: string;
-  dialogType?: "ALERT" | "CONFIRM";
+  text: string;
+  radius?: ShapeSizeType;
+  type: DialogType;
 }
 
 export const DodamDialog = ({
   title,
-  content,
-  dialogType = "ALERT",
+  text,
+  radius = "ExtraLarge",
+  type,
 }: DodamDialogProps) => {
-  return <></>;
+  return (
+    <StyledDialog radius={radius} dialogType={type.dialog}>
+      <Column rowGap={12} padding={type.dialog === "CONFIRM" ? "6px" : "12px"}>
+        <DodamTitle fontScale="Large" text={title} customStyle={StyledTitle} />
+        <DodamBody text={text} customStyle={StyledText} />
+      </Column>
+
+      {type.dialog === "CONFIRM" ? (
+        <Row columnGap={8}>
+          <DodamFilledButton
+            customStyle={type.dismiss.style}
+            onClick={type.dismiss.onClick}
+            radius="Medium"
+          >
+            {type.dismiss.content}
+          </DodamFilledButton>
+          <DodamFilledButton
+            customStyle={type.confirm.style}
+            onClick={type.confirm.onClick}
+            radius="Medium"
+          >
+            {type.confirm.content}
+          </DodamFilledButton>
+        </Row>
+      ) : (
+        <Row justifyContent="flex-end">
+          <DodamBody
+            fontScale="Large"
+            text={type.close.content}
+            onClick={type.close.onClick}
+            customStyle={type.close.style}
+          />
+        </Row>
+      )}
+    </StyledDialog>
+  );
 };
+
+const StyledDialog = styled.div<{
+  radius: ShapeSizeType;
+  dialogType: "ALERT" | "CONFIRM";
+}>`
+  min-width: 280px;
+  max-width: 560px;
+
+  background-color: ${({ theme }) => theme.surfaceContainerHigh};
+  padding: ${({ dialogType }) => (dialogType === "ALERT" ? "12px" : "18px")};
+  border: 1px solid #ddd;
+
+  ${({ radius }) => DodamShape[radius]}
+  ${({ dialogType }) =>
+    FlexLayout({
+      flexDirection: "column",
+      rowGap: dialogType === "CONFIRM" ? "18px" : "24px",
+    })}
+`;
+
+const StyledTitle = css`
+  color: ${({ theme }) => theme.onSurface};
+`;
+
+const StyledText = css`
+  color: ${({ theme }) => theme.tertiary};
+`;
