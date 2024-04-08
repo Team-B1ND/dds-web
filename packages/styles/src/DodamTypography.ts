9ffcc5e1adf3ec021360f036a0eac03e90f59b0a
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-04-08 11:27:34 +0900

    refactor, feat(styles): font-family에 important 옵션 추가

--- packages/styles/src/DodamTypography.ts
+++ packages/styles/src/DodamTypography.ts
@@ -16,68 +16,68 @@ export type TypographyStyles = {
 export const DodamTypography: TypographyStyles = Object.freeze({
   Headline: {
     Large: css`
-      font-family: "Suit-ExtraBold";
+      font-family: "Suit-ExtraBold" !important;
       font-size: 32px;
       line-height: 40px;
     `,
     Medium: css`
-      font-family: "Suit-Bold";
+      font-family: "Suit-Bold" !important;
       font-size: 28px;
       line-height: 32px;
     `,
     Small: css`
-      font-family: "Suit-Bold";
+      font-family: "Suit-Bold" !important;
       font-size: 24px;
       line-height: 32px;
     `,
   },
   Title: {
     Large: css`
-      font-family: "Suit-Bold";
+      font-family: "Suit-Bold" !important;
       font-size: 22px;
       line-height: 28px;
     `,
     Medium: css`
-      font-family: "Suit-Bold";
+      font-family: "Suit-Bold" !important;
       font-size: 18px;
       line-height: 26px;
     `,
     Small: css`
-      font-family: "Suit-Bold";
+      font-family: "Suit-Bold" !important;
       font-size: 16px;
       line-height: 24px;
     `,
   },
   Body: {
     Large: css`
-      font-family: "Suit-SemiBold";
+      font-family: "Suit-SemiBold" !important;
       font-size: 18px;
       line-height: 26px;
     `,
     Medium: css`
-      font-family: "Suit-SemiBold";
+      font-family: "Suit-SemiBold" !important;
       font-size: 16px;
       line-height: 24px;
     `,
     Small: css`
-      font-family: "Suit-Medium";
+      font-family: "Suit-Medium" !important;
       font-size: 14px;
       line-height: 20px;
     `,
   },
   Label: {
     Large: css`
-      font-family: "Suit-Medium";
+      font-family: "Suit-Medium" !important;
       font-size: 14px;
       line-height: 20px;
     `,
     Medium: css`
-      font-family: "Suit-Medium";
+      font-family: "Suit-Medium" !important;
       font-size: 12px;
       line-height: 16px;
     `,
     Small: css`
-      font-family: "Suit-Normal";
+      font-family: "Suit-Normal" !important;
       font-size: 11px;
       line-height: 16px;
     `,
