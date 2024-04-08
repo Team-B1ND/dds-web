5a0a825a7fef9e41e56f5c2cec8725aa91834019
백승하 <qortmdgk12@naver.com>
2024-04-08 20:47:27 +0900

    feat: util dateTransform 추가 (#6)
    * feat: datTransform 추가
    
    * feat: customDate 추가
    
    * feat: fullDateWithAll, fullDateWithHourAndSecond 추가 및 customDate 수정
    
    * chore: fullDateType 추가
    
    * chore: fullDate 연산자 추가

---
+++ packages/utils/src/dateTransform.ts
@@ -0,0 +1,28 @@
+import dayjs, { ConfigType } from "dayjs";
+
+class DateTransform {
+  public hyphen(date?: ConfigType): string {
+    return dayjs(date).format("YYYY-MM-DD");
+  }
+
+  public period(date?: ConfigType): string {
+    return dayjs(date).format("YYYY.MM.DD");
+  }
+
+  public slash(date?: ConfigType): string {
+    return dayjs(date).format("YYYY/MM/DD");
+  }
+
+  public fullDate(
+    fullDateType: "HH" | "HH:mm" | "HH:mm:ss",
+    date?: ConfigType
+  ): string {
+    return dayjs(date).format(`YYYY-MM-DD ${fullDateType || "HH:mm:ss"}`);
+  }
+
+  public customDate(formatStyle: string, date?: ConfigType): string {
+    return dayjs(date).format(formatStyle);
+  }
+}
+
+export default new DateTransform();
