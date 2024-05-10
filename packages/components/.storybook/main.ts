fab7109f4f90036662a321e1d106f354d2e92802
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-05-10 23:45:13 +0900

    refactor: change storybook/react-webpack5 to storybook/react-vite

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -1,9 +1,8 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-webpack5-compiler-swc",
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -11,11 +10,12 @@ const config: StorybookConfig = {
     "@storybook/addon-interactions",
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: "@storybook/react-vite",
     options: {},
   },
   docs: {
     autodocs: "tag",
   },
 };
+
 export default config;
