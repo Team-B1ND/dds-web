911a9ddf4c9d17fae53e3d504e23dfdefa74be4e
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-07-11 08:44:54 +0900

    refactor(components, ui): Erroboundary public 접근제한자 제거

--- packages/components/src/ui/ErrorBoundary/DodamErrorBoundary.tsx
+++ packages/components/src/ui/ErrorBoundary/DodamErrorBoundary.tsx
@@ -13,19 +13,19 @@ export class DodamErrorBoundary extends Component<
   DodamErrorBoundaryProps,
   DodamErrorBoundaryState
 > {
-  public state: DodamErrorBoundaryState = {
+  state: DodamErrorBoundaryState = {
     hasError: false,
   };
 
-  public static getDerivedStateFromError(_: Error): DodamErrorBoundaryState {
+  static getDerivedStateFromError(_: Error): DodamErrorBoundaryState {
     return { hasError: true };
   }
 
-  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     console.error("Uncaught error:", error, errorInfo);
   }
 
-  public render() {
+  render() {
     if (this.state.hasError) {
       return this.props.fallback;
     }
