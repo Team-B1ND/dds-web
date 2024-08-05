d839e1f3683e50c48e15b3c137c143f15b6b1275
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-08-05 23:47:35 +0900

    refactor: IconProps extends SVGAttributes<HTMLOrSVGElement> -> ComponentProps<"svg">로 변경

--- packages/assets/src/icons/type.ts
+++ packages/assets/src/icons/type.ts
@@ -1,7 +1,7 @@
-import type { SVGAttributes } from "react";
+import type { ComponentProps, SVGAttributes } from "react";
 import type { RuleSet } from "styled-components";
 
-export interface IconProps extends SVGAttributes<HTMLOrSVGElement> {
+export interface IconProps extends ComponentProps<"svg"> {
   size?: number;
   svgStyle?: RuleSet;
   pathStyle?: RuleSet;
