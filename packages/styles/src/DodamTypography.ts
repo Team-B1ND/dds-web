f90abd7fffd4520f06e334716915864ec2a2ee3a
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-09-08 19:22:25 +0900

    refactor :: DodamTypography #18

--- packages/styles/src/DodamTypography.ts
+++ packages/styles/src/DodamTypography.ts
@@ -22,23 +22,20 @@ export type TypographyStyles = {
 export const DodamTypography: TypographyStyles = Object.freeze({
   Title1: {
     Bold: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Bold";
       font-size: 36px;
-      font-weight: 800;
       line-height: 130%;
       letter-spacing: -1.08px;
     `,
     Medium: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Medium";
       font-size: 36px;
-      font-weight: 500;
       line-height: 130%;
       letter-spacing: -1.08px;
     `,
     Regular: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Regular";
       font-size: 36px;
-      font-weight: 400;
       line-height: 130%;
       letter-spacing: -1.08px;
     `,
@@ -46,23 +43,20 @@ export const DodamTypography: TypographyStyles = Object.freeze({
 
   Title2: {
     Bold: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Bold";
       font-size: 28px;
-      font-weight: 800;
       line-height: 130%;
       letter-spacing: -0.84px;
     `,
     Medium: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Medium";
       font-size: 28px;
-      font-weight: 500;
       line-height: 130%;
       letter-spacing: -0.84px;
     `,
     Regular: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Regular";
       font-size: 28px;
-      font-weight: 400;
       line-height: 130%;
       letter-spacing: -0.84px;
     `,
@@ -70,23 +64,20 @@ export const DodamTypography: TypographyStyles = Object.freeze({
 
   Title3: {
     Bold: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Bold";
       font-size: 24px;
-      font-weight: 800;
       line-height: 130%;
       letter-spacing: -0.48px;
     `,
     Medium: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Medium";
       font-size: 24px;
-      font-weight: 500;
       line-height: 130%;
       letter-spacing: -0.48px;
     `,
     Regular: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Regular";
       font-size: 24px;
-      font-weight: 400;
       line-height: 130%;
       letter-spacing: -0.48px;
     `,
@@ -94,23 +85,20 @@ export const DodamTypography: TypographyStyles = Object.freeze({
 
   Heading1: {
     Bold: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Bold";
       font-size: 22px;
-      font-weight: 800;
       line-height: 130%;
       letter-spacing: -0.44px;
     `,
     Medium: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Medium";
       font-size: 22px;
-      font-weight: 500;
       line-height: 130%;
       letter-spacing: -0.44px;
     `,
     Regular: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Regular";
       font-size: 22px;
-      font-weight: 400;
       line-height: 130%;
       letter-spacing: -0.44px;
     `,
@@ -118,23 +106,20 @@ export const DodamTypography: TypographyStyles = Object.freeze({
 
   Heading2: {
     Bold: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Bold";
       font-size: 20px;
-      font-weight: 800;
       line-height: 140%;
       letter-spacing: -0.2px;
     `,
     Medium: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Medium";
       font-size: 20px;
-      font-weight: 500;
       line-height: 140%;
       letter-spacing: -0.2px;
     `,
     Regular: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Regular";
       font-size: 20px;
-      font-weight: 400;
       line-height: 140%;
       letter-spacing: -0.2px;
     `,
@@ -142,43 +127,37 @@ export const DodamTypography: TypographyStyles = Object.freeze({
 
   Headline: {
     Bold: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Bold";
       font-size: 18px;
-      font-weight: 700;
     `,
     Medium: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Medium";
       font-size: 18px;
-      font-weight: 500;
       line-height: 150%;
     `,
     Regular: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Regular";
       font-size: 18px;
-      font-weight: 800;
       line-height: 150%;
     `,
   },
 
   Body: {
     Bold: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Bold";
       font-size: 16px;
-      font-weight: 700;
       line-height: 150%;
       letter-spacing: -0.16px;
     `,
     Medium: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Medium";
       font-size: 16px;
-      font-weight: 500;
       line-height: 150%;
       letter-spacing: -0.16px;
     `,
     Regular: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Regular";
       font-size: 16px;
-      font-weight: 400;
       line-height: 150%;
       letter-spacing: -0.16px;
     `,
@@ -186,23 +165,20 @@ export const DodamTypography: TypographyStyles = Object.freeze({
 
   Label: {
     Bold: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Bold";
       font-size: 14px;
-      font-weight: 600;
       line-height: 140%;
       letter-spacing: 0.28px;
     `,
     Medium: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Medium";
       font-size: 14px;
-      font-weight: 500;
       line-height: 140%;
       letter-spacing: 0.28px;
     `,
     Regular: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Regular";
       font-size: 14px;
-      font-weight: 400;
       line-height: 140%;
       letter-spacing: 0.28px;
     `,
@@ -210,23 +186,20 @@ export const DodamTypography: TypographyStyles = Object.freeze({
 
   Caption1: {
     Bold: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Bold";
       font-size: 13px;
-      font-weight: 600;
       line-height: 130%;
       letter-spacing: 0.39px;
     `,
     Medium: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Medium";
       font-size: 13px;
-      font-weight: 500;
       line-height: 130%;
       letter-spacing: 0.39px;
     `,
     Regular: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Regular";
       font-size: 13px;
-      font-weight: 400;
       line-height: 130%;
       letter-spacing: 0.39px;
     `,
@@ -234,23 +207,20 @@ export const DodamTypography: TypographyStyles = Object.freeze({
 
   Caption2: {
     Bold: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Bold";
       font-size: 12px;
-      font-weight: 600;
       line-height: 130%;
       letter-spacing: 0.36px;
     `,
     Medium: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Medium";
       font-size: 12px;
-      font-weight: 500;
       line-height: 130%;
       letter-spacing: 0.36px;
     `,
     Regular: css`
-      font-family: "Pretendard";
+      font-family: "Pretendard-Regular";
       font-size: 12px;
-      font-weight: 400;
       line-height: 130%;
       letter-spacing: 0.36px;
     `,
