af9775f022267c4382eabba1b424c998ea59433b
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-12-19 17:00:58 +0900

    remove :: 안쓰는 폴더 삭제 #36

--- packages/components/src/ui/index.ts
+++ packages/components/src/ui/index.ts
@@ -3,6 +3,5 @@ export * from "./DatePicker";
 export * from "./Dialog";
 export * from "./ErrorBoundary";
 export * from "./NotFoundPage";
-export * from "./ProgressIndicator";
 export * from "./TextField";
 export * from "./Typography";
