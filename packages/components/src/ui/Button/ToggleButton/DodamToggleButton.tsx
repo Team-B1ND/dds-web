a2e5d922d7f7f397b38e61c0d4b3e8cc6903fcc7
전민찬 <chan2bo2@dgsw.hs.kr>
2025-02-02 19:11:57 +0900

    fix :: version && byproduct DodamToggleButton #48

---
+++ packages/components/src/ui/Button/ToggleButton/DodamToggleButton.tsx
@@ -0,0 +1,44 @@
+import React, { MouseEventHandler } from 'react';
+import styled from 'styled-components';
+import { DodamShape, DodamThemeProvider } from '@dds-web/styles';
+
+export interface ToggleBtnProps {
+  isAtv: boolean;
+  onclick?: MouseEventHandler<HTMLDivElement>;
+}
+
+export const DodamToggleButton = ({ isAtv, onclick }: ToggleBtnProps) => {
+  return (
+    <StyledToggleButtonWrap isAtv={isAtv} onClick={onclick}>
+      <StyledToggleButton isAtv={isAtv}></StyledToggleButton>
+    </StyledToggleButtonWrap>
+  );
+};
+
+const StyledToggleButtonWrap = styled.div<{ isAtv: boolean }>`
+  width: 50px;
+  height: 30px;
+
+  display: flex;
+  align-items: center;
+
+  border-radius: 15px;
+  background-color: ${({ isAtv, theme }) => (isAtv ? theme.primaryNormal : theme.labelDisabled)};
+
+  position: relative;
+  cursor: pointer;
+`;
+
+const StyledToggleButton = styled.span<{ isAtv: boolean }>`
+  width: 25px;
+  height: 25px;
+
+  background-color: ${({ theme }) => theme.staticWhite};
+
+  transition: 0.2s ease-out;
+  left: ${({ isAtv }) => (isAtv ? '38%' : '0')};
+  border-radius: 50%;
+
+  margin: 0 3px;
+  position: absolute;
+`;
