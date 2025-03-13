88c86ee26692ed410f946c33f8cec1adbef59dc7
developersihyeon <sihyeonsky@gmail.com>
2025-03-13 17:45:05 +0900

    feat :: DodamShadow 다크모드 적용 #114

--- packages/styles/src/DodamShadow.ts
+++ packages/styles/src/DodamShadow.ts
@@ -1,16 +1,29 @@
 import { css } from "styled-components";
+import { hexToRgba } from "@dds-web/utils";
 
 export const DodamShadow = Object.freeze({
-    Normal: css`
-        box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.12), 0px 0px 1px 0px rgba(0, 0, 0, 0.08), 0px 0px 1px 0px rgba(0, 0, 0, 0.08);
-    `,
-    Emphasize: css`
-        box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.12), 0px 1px 4px 0px rgba(0, 0, 0, 0.08), 0px 0px 1px 0px rgba(0, 0, 0, 0.08);
-    `,
-    Strong: css`
-        box-shadow: 0px 6px 12px 0px rgba(0, 0, 0, 0.12), 0px 4px 8px 0px rgba(0, 0, 0, 0.08), 0px 0px 4px 0px rgba(0, 0, 0, 0.08);
-    `,
-    Heavy: css`
-        box-shadow: 0px 16px 20px 0px rgba(0, 0, 0, 0.12), 0px 8px 16px 0px rgba(0, 0, 0, 0.08), 0px 0px 8px 0px rgba(0, 0, 0, 0.08);
-    `,
+  Normal: css`
+    box-shadow:
+      0px 1px 2px 0px ${({ theme }) => hexToRgba(theme.labelStrong, 0.12)},
+      0px 0px 1px 0px ${({ theme }) => hexToRgba(theme.labelStrong, 0.08)},
+      0px 0px 1px 0px ${({ theme }) => hexToRgba(theme.labelStrong, 0.08)};
+  `,
+  Emphasize: css`
+    box-shadow:
+      0px 2px 8px 0px ${({ theme }) => hexToRgba(theme.labelStrong, 0.12)},
+      0px 1px 4px 0px ${({ theme }) => hexToRgba(theme.labelStrong, 0.08)},
+      0px 0px 1px 0px ${({ theme }) => hexToRgba(theme.labelStrong, 0.08)};
+  `,
+  Strong: css`
+    box-shadow:
+      0px 6px 12px 0px ${({ theme }) => hexToRgba(theme.labelStrong, 0.12)},
+      0px 4px 8px 0px ${({ theme }) => hexToRgba(theme.labelStrong, 0.08)},
+      0px 0px 4px 0px ${({ theme }) => hexToRgba(theme.labelStrong, 0.08)};
+  `,
+  Heavy: css`
+    box-shadow:
+      0px 16px 20px 0px ${({ theme }) => hexToRgba(theme.labelStrong, 0.12)},
+      0px 8px 16px 0px ${({ theme }) => hexToRgba(theme.labelStrong, 0.08)},
+      0px 0px 8px 0px ${({ theme }) => hexToRgba(theme.labelStrong, 0.08)};
+  `,
 });
